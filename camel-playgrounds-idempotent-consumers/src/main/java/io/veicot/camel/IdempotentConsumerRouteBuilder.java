4cc0c04ef1670888617dd5922b77a30f1fe618a8
Leandro Beretta <lea.beretta@gmail.com>
2017-12-03 12:19:33 -0300

    some improvements

--- camel-playgrounds-idempotent-consumers/src/main/java/io/veicot/camel/IdempotentConsumerRouteBuilder.java
+++ camel-playgrounds-idempotent-consumers/src/main/java/io/veicot/camel/IdempotentConsumerRouteBuilder.java
@@ -1,13 +1,7 @@
 package io.veicot.camel;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.processor.idempotent.jdbc.JdbcMessageIdRepository;
-import org.apache.camel.spi.IdempotentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @Component
 public class IdempotentConsumerRouteBuilder extends RouteBuilder {
@@ -15,8 +9,7 @@ public class IdempotentConsumerRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("file:./src/data?idempotent=true&idempotentRepository=#idempotentRepository")
-                .log("${body}")
+        from("file:{{camel.playgrounds.idempotentconsumers.dir}}?idempotent=true&idempotentRepository=#idempotentRepository")
                 .log("${header.CamelFileName}");
     }
 }
